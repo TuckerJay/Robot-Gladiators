27ff91a3c93c5033f29899b53b9db728a2f33a4a
TuckerJD <tucker.j.dalrymple@gmail.com>
2022-06-14 20:18:10 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,7 +34,7 @@ var fight = function(enemyName) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
 
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney ", playerMoney);
 
                 break;
@@ -47,7 +47,8 @@ var fight = function(enemyName) {
         if (promptFight === "fight" || promptFight === "FIGHT") {
 
             // remove enemy's health by subtracting the amount set in the playerAttack variable
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
@@ -68,7 +69,8 @@ var fight = function(enemyName) {
 
   
             // remove player's health by subtracting the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
@@ -102,7 +104,7 @@ var startGame = function() {
     //reset player stats
     playerHealth = 100;
 
-    playerAttack = 30;
+    playerAttack = 10;
 
     playerMoney = 10;
 
@@ -114,7 +116,7 @@ var startGame = function() {
     
             var pickedEnemyName = enemyNames[i];
     
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
         
             fight(pickedEnemyName);
             
@@ -235,6 +237,16 @@ var shop = function() {
     }
 
 };
+
+
+// random number generator
+var randomNumber = function(min, max) {
+
+    var value = Math.floor(Math.random() *(max - min + 1)) + min;
+
+    return value;
+
+}
 
 
 // start the game when the page loads
