4fcf61c7811b2949a338f971b24c44b9926b5fee
TuckerJD <tucker.j.dalrymple@gmail.com>
2022-06-14 22:54:29 -0500

    high score mechanic added

--- assets/js/game.js
+++ assets/js/game.js
@@ -172,19 +172,40 @@ var endGame = function() {
     // Player Win
     if (playerInfo.health > 0) {
 
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("Great job, you've survived the game! You now had a score of " + playerInfo.money + ". Let's see how you did!");
+
+        // check localStorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+
+        // if player has more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+
+        } 
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
 
     }
+
     else {
 
         window.alert("You've lost your robot in battle.")
 
     }
 
-    // Play again query
-    var playAgainConfrim = window.confirm("Would you like to play again?");
 
-    if (playAgainConfrim) {
+    // Play again query
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
 
         //restart game
         startGame();
