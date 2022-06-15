230496e13e716857ee1320b018a23a518c6c19c5
TuckerJD <tucker.j.dalrymple@gmail.com>
2022-06-14 22:14:41 -0500

    shop int inputs

--- assets/js/game.js
+++ assets/js/game.js
@@ -182,28 +182,28 @@ var shop = function() {
 
     // ask player shop choice
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+
+    // string to int converter
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // Shop switch
     switch (shopOptionPrompt) {
 
-        case "REFILL":
-        case "refill":
+        case 1:
             
             playerInfo.refillHealth();
 
             break;
 
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             
             playerInfo.upgradeAttack();
 
             break;
 
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             break;
